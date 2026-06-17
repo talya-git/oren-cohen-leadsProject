b6e71ebe9e37076c57e35cf1b52fd6152c799585
talya toledano <talyatoledano10@gmail.com>
2026-06-17 14:07:57 +0300

    fix: enable Swagger in production

--- LeadsProject/Program.cs
+++ LeadsProject/Program.cs
@@ -96,6 +96,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
