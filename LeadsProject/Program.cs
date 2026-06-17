47f5a4b4a906bb58e64e8dc1f8848b47fbd1357e
talya toledano <talyatoledano10@gmail.com>
2026-06-17 13:57:20 +0300

    fix: default port 5432 when not specified in postgres URL

--- LeadsProject/Program.cs
+++ LeadsProject/Program.cs
@@ -14,7 +14,8 @@ if (connStr.StartsWith("postgres://") || connStr.StartsWith("postgresql://"))
 {
     var uri = new Uri(connStr);
     var userInfo = uri.UserInfo.Split(':');
-    connStr = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    connStr = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 
 if (connStr.Contains("Host=") || connStr.Contains("host="))
